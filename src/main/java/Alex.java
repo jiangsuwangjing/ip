4eacca29e4686e8d922dab20e5a06bd290d6d03d
jiangsuwangjing <wzmoscar@gmail.com>
2025-01-23 20:48:15 +0800

    Renamed Duke to Alex. Added greeting and exit messages.

---
+++ src/main/java/Alex.java
@@ -0,0 +1,9 @@
+public class Alex {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Alex.\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
